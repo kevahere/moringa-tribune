c1113bbd02acf7a7faca427280a7d6a69930f269
kevahere <kevahere@gmail.com>
2018-10-30 14:15:35 +0300

    added tests for editors

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Editor,Article,tags
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+
+    #Set up method
+    def setUp(self):
+        self.kevin = Editor(first_name='Kevin', last_name ='Ahere',email = 'kevahere@gmail.com')
+
+#Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.kevin,Editor))
+#Testing save method
+
+    def test_save_method(self):
+        self.kevin.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
